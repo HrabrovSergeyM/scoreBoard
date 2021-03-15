8c6084f57a21f69d01b438af3c26017a988cb195
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-03-15 05:12:57 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 import App from "./components/App";
 import "./index.css";
 
-
-ReactDOM.render((
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <App />
-    </BrowserRouter>
- ), document.getElementById("root"))  
+ReactDOM.render(
+  <HashRouter hashType={"slash"}>
+    <App />
+  </HashRouter>,
+  document.getElementById("root")
+);
